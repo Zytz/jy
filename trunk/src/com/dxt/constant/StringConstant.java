60a7d3499221b2d1aa8c5334664dbe2f480e741e
wenwei8268 <wenwei8268@6c082b38-74e6-4642-9b2c-2ba680a29e27>
2015-03-12 02:54:54 +0000

    modify StringCOnstant

--- trunk/src/com/dxt/constant/StringConstant.java
+++ trunk/src/com/dxt/constant/StringConstant.java
@@ -2,8 +2,9 @@ package com.dxt.constant;
 
 public class StringConstant {
 	public static final String PROTOCOL = "http://";
-	public static final String IP = "10.82.21.166";
+	//public static final String IP = "10.82.21.166";
 	public static final String PORT = "8080";
 	public static final String APPNAME = "daxuetong";
 	public static final String SERVICE = "service";
+	public static final String SERVICE_URL ="http://210.40.65.224:8080/daxuetong/";
 }
